5a191b9db2913d90fff9ef90a9779cfbe46a498c
janine c <janine.cheng@gmail.com>
2016-05-11 16:57:53 -0700

    added population and gps_height imputation 	modified:   DEA_jcc.ipynb 	modified:   DEA_jcc.py

--- MVP_work/DEA_jcc.py
+++ MVP_work/DEA_jcc.py
@@ -7,6 +7,10 @@
 
 # <codecell>
 
+import numpy as np
+
+# <codecell>
+
 # use pandas to read in data
 import pandas as pd
 
@@ -56,28 +60,44 @@ df.head()
 
 # <codecell>
 
-cols = list(df.columns.values)
+def ready_for_model(df):
+    cols = list(df.columns.values)
 
-# keep columns
-cols_keep = []
-for c in df:
-    if df[c].dtype in [int, float]:
-        cols_keep.append(c)
-    elif df[c].dtype == object:
-        if df[c].nunique() < 20:
+    # keep columns
+    cols_keep = []
+    cols_giveup = []
+    for c in df:
+        if df[c].dtype in [int, float]:
             cols_keep.append(c)
+        elif df[c].dtype == object:
+            if df[c].nunique() < 25:
+                cols_keep.append(c)
+            else:
+                cols_giveup.append(c)
 
-# remove the labels
-for to_remove in ['id', 'status', 'status_group']:
-    cols_keep.remove(to_remove)
+    # remove the labels
+    for to_remove in ['id', 'status', 'status_group']:
+        cols_keep.remove(to_remove)
 
-# convert df to X, y by patsy
-r_formula = 'status ~' + ' + '.join(cols_keep)
-df_y, df_X = patsy.dmatrices(r_formula, df, return_type='dataframe')
+    # convert df to X, y by patsy
+    r_formula = 'status ~' + ' + '.join(cols_keep)
+    df_y, df_X = patsy.dmatrices(r_formula, df, return_type='dataframe')
 
-cols_X = df_X.columns
-X = df_X.values
-y = df_y.values
+    cols_X = df_X.columns
+    X = df_X.values
+    y = df_y.values
+    return (X, y, cols_X, r_formula, cols_keep, cols_giveup)
+
+X, y, cols_X, r_formula, cols_keep, cols_giveup = ready_for_model(df)
+
+# <codecell>
+
+r_formula
+
+# <codecell>
+
+# we gave up these features
+cols_giveup
 
 # <codecell>
 
@@ -130,9 +150,40 @@ for train_index, test_index in KFold(len(y), n_folds=5):
     model.fit(X_train, y_train)
     print model.score(X_test, y_test)
 
+# <codecell>
 
-# <markdown>
-# ### so far, we only used the automatic feature selection.
+from sklearn.metrics import confusion_matrix
+# get y_pred and y_actual
+y_pred = model.predict(X_test)
+y_actual = y_test
+
+# convert y's to binary
+
+# y_pred_bin = ~y_pred.astype(bool)  
+# y_actual_bin = ~y_actual.astype(bool)  #
+
+# we want True means not-functional or need-repair (where the govern. needs to send people) , False: functional
+# In our data, 2: functional 1: need repair 0: non-functional
+y_pred_bin = (y_pred < 2)
+y_actual_bin = (y_actual < 2)
+
+conf = confusion_matrix(y_actual_bin, y_pred_bin)
+conf
+
+# <codecell>
+
+TP = conf[0, 0]
+FN = conf[0, 1]
+FP = conf[1, 0]
+TN = conf[1, 1]
+recall = TP * 1./ (TP + FN)
+print "recall", recall
+precision = TP * 1./ (TP + FP)
+print "precision", precision
+
+# <markdowncell>
+
+# so far, we only used the automatic feature selection.
 # To get some idea of what feature really matters, we can check
 # the importance of features
 
@@ -140,18 +191,116 @@ for train_index, test_index in KFold(len(y), n_folds=5):
 
 import numpy as np
 import matplotlib.pyplot as plt
+%matplotlib inline
 
+# in tree and ensemble type of models, we can use feature_importances_
 importances = model.feature_importances_
 std = np.std([tree.feature_importances_ for tree in model.estimators_], axis=0)
 indices = importances.argsort()[::-1]
+print "Features ranked by importance:"
+for i, (feature, importance) in enumerate(zip(cols_X[indices], importances[indices])):
+    print i, feature, importance
 
+# <codecell>
+
+importances.sum()
+
+# <codecell>
+
+# just plot the first 20
 n_first = 20
 plt.figure(figsize=[15, 10])
-plt.bar(range(n_first), importances[indices[0:20]],
+plt.bar(range(n_first), importances[indices[0:20]], color='g',
         yerr=std[indices[0:20]], align='center')
-plt.xticks(range(n_first), cols_X[indices[0:20]], rotation=90)
+plt.xticks(range(n_first), cols_X[indices[0:20]], rotation=60)
 plt.tight_layout()
 plt.show()
+
+# <markdowncell>
+
+# ### Now, let's do some imputation
+# 
+# for population, we'll first try to fill in missing value with the mean of groups
+
+# <codecell>
+
+# it is copied from Ash's code
+# firstly, replace 
+dat = df.copy()
+dat.population.replace(to_replace={0: np.nan}, inplace=True)
+
+# <codecell>
+
+#let's look at population by imputing with mean population in neighboring areas, in this order:
+#sub-village > ward > lga > region_code
+
+dat.population.fillna(dat.groupby(['subvillage'])['population'].transform('mean'), inplace=True)
+dat.population.fillna(dat.groupby(['ward'])['population'].transform('mean'), inplace=True)
+dat.population.fillna(dat.groupby(['lga'])['population'].transform('mean'), inplace=True)
+dat.population.fillna(dat.groupby(['region_code'])['population'].transform('mean'), inplace=True)
+
+# <codecell>
+
+X, y, cols_X, r_formula, cols_keep, cols_giveup = ready_for_model(dat)
+
+# <codecell>
+
+for model in [LogisticRegression(), DecisionTreeClassifier(),
+              KNeighborsClassifier(), GaussianNB(), RandomForestClassifier()]:
+    split_n_fit(model, X, y)
+
+# <codecell>
+
+
+# <codecell>
+
+# it looks RandomForest does us better, let's try a new parameter
+
+model = RandomForestClassifier(n_estimators=200)
+split_n_fit(model, X, y)
+
+# <codecell>
+
+# it looks RandomForest does us better, let's try a new parameter
+
+model = RandomForestClassifier(n_estimators=300)
+split_n_fit(model, X, y)
+
+# <codecell>
+
+## LOCATION
+# gps_height
+
+(dat.gps_height == 0).sum()/float(len(dat.gps_height))
+
+# <codecell>
+
+dat = df.copy()
+dat.gps_height.replace(to_replace={0: np.nan}, inplace=True)
+#let's look at gps_height by imputing with mean gps_height in neighboring areas, in this order:
+#sub-village > ward > lga > region_code
+
+dat.gps_height.fillna(dat.groupby(['subvillage'])['gps_height'].transform('mean'), inplace=True)
+dat.gps_height.fillna(dat.groupby(['ward'])['gps_height'].transform('mean'), inplace=True)
+dat.gps_height.fillna(dat.groupby(['lga'])['gps_height'].transform('mean'), inplace=True)
+dat.gps_height.fillna(dat.groupby(['region_code'])['gps_height'].transform('mean'), inplace=True)
+
+# <codecell>
+
+X, y, cols_X, r_formula, cols_keep, cols_giveup = ready_for_model(dat)
+
+# <codecell>
+
+for model in [LogisticRegression(), DecisionTreeClassifier(),
+              KNeighborsClassifier(), GaussianNB(), RandomForestClassifier()]:
+    split_n_fit(model, X, y)
+
+# <codecell>
+
+# it looks RandomForest does us better, let's try a new parameter
+
+model = RandomForestClassifier(n_estimators=200)
+split_n_fit(model, X, y)
 
 # <codecell>
 
